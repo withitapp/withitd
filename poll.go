d2d0879968d97e0151e44a8a2a732759c20fd10b
scrubsauce <millerjj@usc.edu>
2014-02-01 12:19:35 -0800

    Add Poll struct

---
+++ poll.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"time"
+)
+
+// Struct to represent a poll for the app
+type Poll struct {
+	ID          int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	Title       string
+	Description string
+	UserID      int
+	EndsAt      time.Time
+}
+
+// TODO Implement Poll.MembershipIDs() []int
