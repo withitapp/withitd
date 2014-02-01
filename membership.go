583b858bc8358bcebe2b4bf672967b7948734912
scrubsauce <millerjj@usc.edu>
2014-02-01 12:25:53 -0800

    Add Membership struct

---
+++ membership.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"time"
+)
+
+//Struct to represent user memberships in polls
+type Membership struct {
+	ID        int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	UserID    int
+	PollID    int
+	Response  bool
+}
