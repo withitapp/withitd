3a2390b7a8c511fe5eda02e7120f4e2e3fb29d7c
Oguz Bilgic <fisyonet@gmail.com>
2014-02-10 14:03:17 -0500

    Add http routing to main()

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	println("Hello WithIt App")
+	http.HandleFunc("/users/", getUserHandler)
+	fmt.Println("Starting http server at port 3000 ...")
+	http.ListenAndServe(":3000", nil)
 }
