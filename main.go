743293afd3241a6e7071844255435270fa1ce547
Oguz Bilgic <fisyonet@gmail.com>
2014-02-13 02:32:54 -0500

    Create controller.go file

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/withitapp/withitd/cntrl"
 	"github.com/withitapp/withitd/dbase"
 	"net/http"
-	"net/url"
-	"strconv"
 	"time"
 )
 
@@ -42,53 +39,5 @@ func (ah *APPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		if path == r.URL.Path {
 			handler.ServeHTTP(w, r)
 		}
-	}
-}
-
-type Controller interface {
-	Index() interface{}
-	Show(ID int) (interface{}, error)
-	Create(values url.Values) (int, error)
-	Update(ID int, values url.Values) error
-	Delete(ID int) error
-}
-
-func NewControllerHandler(controller Controller) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		idString := r.URL.Query().Get("id")
-		id, err := strconv.Atoi(idString)
-		if err != nil {
-			panic(err)
-		}
-
-		var response interface{}
-
-		switch r.Method {
-		case "GET":
-			if id != 0 {
-				response, err = controller.Show(id)
-			} else {
-				response = controller.Index()
-			}
-		case "PUT", "POST":
-			id, err = controller.Create(r.PostForm)
-		case "UPDATE":
-			err = controller.Update(id, r.PostForm)
-		case "DELETE":
-			err = controller.Delete(id)
-		default:
-		}
-
-		if err != nil {
-			panic(err)
-		}
-
-		jsonBlob, err := json.Marshal(response)
-		if err != nil {
-			panic(err)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(jsonBlob)
 	}
 }
