2270d87ba877bd0a6fa5b7af8a4f958fe7ff894d
scrubsauce <millerjj@usc.edu>
2014-02-24 16:38:17 -0800

    add Less, Great func

--- vldte/int.go
+++ vldte/int.go
@@ -1,9 +1,21 @@
 package vldte
 
+import (
+	"errors"
+)
+
 func Less(i int, limit int) error {
+	if i >= limit {
+		return errors.New("i is greater than or equal to limit.")
+	}
 	return nil
 }
 
 func Greater(i int, limit int) error {
+
+	if i <= limit {
+		return errors.New("i less than or equal to limit.")
+	}
 	return nil
+
 }
