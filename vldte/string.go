898a0f01be6c25c88ed78a1d9c54b2951382b651
scrubsauce <millerjj@usc.edu>
2014-02-25 20:54:29 -0800

    Add imports

--- vldte/string.go
+++ vldte/string.go
@@ -1,5 +1,10 @@
 package vldte
 
+import (
+	"errors"
+	"regexp"
+)
+
 func Email(str string) error {
 	var validEmail = regexp.MustCompile(`^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$`)
 
