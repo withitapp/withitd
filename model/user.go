d4a767d2642716d0635b6fc289fcd44f719673f0
scrubsauce <millerjj@usc.edu>
2014-02-28 16:24:32 -0800

    implement func Validate

--- model/user.go
+++ model/user.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"time"
+	"github.com/withitapp/withitd/vldte"
 )
 
 // Struct to represent the user of the app and their associated data
@@ -16,6 +17,54 @@ type User struct {
 	FacebookID string
 }
 
-func (u *User) Validate() []error {
+func (u *User) Validate() error {
+	stringLengthMax := 25
+	stringLengthMin := 1
+
+	//Check Username
+	err := vldte.AlphaNumeric(u.Username)
+	if err != nil {
+		return err
+	}
+
+	err = vldte.Length(u.Username, stringLengthMin, stringLengthMax)
+	if err != nil {
+		return err
+	}
+
+	//Check Email
+	err = vldte.Email(u.Email)
+	if err != nil {
+		return err
+	}
+
+	//Check First Name
+	err = vldte.Alphabetic(u.FirstName)
+	if err != nil {
+		return err
+	}
+
+	err = vldte.Length(u.FirstName, stringLengthMin, stringLengthMax)
+	if err != nil {
+		return err
+	}
+
+	//Check Last Name
+	err = vldte.Alphabetic(u.LastName)
+	if err != nil {
+		return err
+	}
+
+	err = vldte.Length(u.LastName, stringLengthMin, stringLengthMax)
+	if err != nil {
+		return err
+	}
+
+	//Check Facebook ID
+	err = vldte.Numeric(u.FacebookID)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
