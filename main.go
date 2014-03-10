4045944661fc110aa574918b6459aa28def80c65
Oguz Bilgic <fisyonet@gmail.com>
2014-03-10 16:25:07 -0400

    Add initial /auth handler

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/huandu/facebook"
 	"github.com/withitapp/withitd/cntrl"
 	"github.com/withitapp/withitd/dbase"
 	"net/http"
@@ -20,6 +22,34 @@ func main() {
 	appHandler := NewAPPHandler()
 	appHandler.AddHandler("/users", NewControllerHandler(userController))
 	appHandler.AddHandler("/polls", NewControllerHandler(pollController))
+	appHandler.AddHandler("/auth", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		data := r.PostForm
+		id := data["fb_id"][0]
+		token := data["fb_token"][0]
+
+		fmt.Println(id, token)
+
+		fbApp := facebook.New("514907081964376", "39cb26381e1cb82ae689ff1d7755f577")
+		fbApp.RedirectUri = "http://withitapp.com/"
+
+		session := fbApp.Session(token)
+
+		res, err := session.Get("/me/feed", nil)
+
+		var name string
+		res.DecodeField("first_name", &name)
+		if err != nil {
+			panic(err)
+		}
+
+		err = session.Validate()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(name)
+	})
 
 	server := &http.Server{
 		Addr:           ":3000",
