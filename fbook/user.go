5785aad6a21608f531634beda637a94afcb32448
scrubsauce <millerjj@usc.edu>
2014-03-28 15:25:05 -0700

    Add facebook tags to fbook.user

--- fbook/user.go
+++ fbook/user.go
@@ -7,27 +7,27 @@ import (
 */
 
 type User struct {
-	ID  string `json:"id"`
-	Bio string `json:"bio"`
+	ID  string `json:"id", facebook:"id"`
+	Bio string `json:"bio", facebook:"bio"`
 	//Birthday           time.Time `json:"birthday"`
-	Birthday           string `json:"birthday"`
-	Email              string `json:"email"`
-	FirstName          string `json:"first_name"`
-	MiddleName         string `json:"middle_name"`
-	LastName           string `json:"last_name"`
-	Name               string `json:"name"`
-	NameFormat         string `json:"name_format"`
-	Gender             string `json:"gender"`
-	Installed          bool   `json:"installed"`
-	IsVerified         bool   `json:"is_verified"`
-	Link               string `json:"link"`
-	Locale             string `json:"locale"`
-	Political          string `json:"political"`
-	Quotes             string `json:"quotes"`
-	RelationshipStatus string `json:"relationship_status"`
-	Religion           string `json:"religion"`
-	ThirdPartyID       string `json:"third_party_id"`
-	Username           string `json:"username"`
-	Verified           bool   `json:"verified"`
-	Website            string `json:"website"`
+	Birthday           string `json:"birthday", facebook:"birthday"`
+	Email              string `json:"email", facebook:"email"`
+	FirstName          string `json:"first_name", facebook: "first_name"`
+	MiddleName         string `json:"middle_name", facebook:"middle_name"`
+	LastName           string `json:"last_name", facebook:"last_name"`
+	Name               string `json:"name", facebook:"name"`
+	NameFormat         string `json:"name_format", facebook:"name_format"`
+	Gender             string `json:"gender", facebook:"gender"`
+	Installed          bool   `json:"installed, facebook:"installed"`
+	IsVerified         bool   `json:"is_verified", facebook:"is_verified"`
+	Link               string `json:"link", facebook:"link"`
+	Locale             string `json:"locale", facebook:"locale"`
+	Political          string `json:"political", facebook:"political"`
+	Quotes             string `json:"quotes", facebook:"quotes"`
+	RelationshipStatus string `json:"relationship_status", facebook:"relationship_status"`
+	Religion           string `json:"religion", facebook:"religion"`
+	ThirdPartyID       string `json:"third_party_id", facebook:"third_party_id"`
+	Username           string `json:"username", facebook:"username"`
+	Verified           bool   `json:"verified", facebook:"verified"`
+	Website            string `json:"website", facebook:"website"`
 }
