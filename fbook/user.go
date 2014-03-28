a396efc485d5543c5687aaedaf590aee51c79c0c
scrubsauce <millerjj@usc.edu>
2014-03-28 15:52:34 -0700

    adjust facebook tag for user struct

--- fbook/user.go
+++ fbook/user.go
@@ -8,25 +8,25 @@ import (
 
 type User struct {
 	ID                 string `json:"id" facebook:"id,required"`
-	Bio                string `json:"bio" facebook:"bio,required"`
-	Birthday           string `json:"birthday" facebook:"birthday,required"`
+	Bio                string `json:"bio" facebook:"bio"`
+	Birthday           string `json:"birthday" facebook:"birthday"`
 	Email              string `json:"email" facebook:"email,required"`
 	FirstName          string `json:"first_name" facebook: "first_name,required"`
-	MiddleName         string `json:"middle_name" facebook:"middle_name,required"`
+	MiddleName         string `json:"middle_name" facebook:"middle_name"`
 	LastName           string `json:"last_name" facebook:"last_name,required"`
-	Name               string `json:"name" facebook:"name,required"`
-	NameFormat         string `json:"name_format" facebook:"name_format,required"`
-	Gender             string `json:"gender" facebook:"gender,required"`
-	Installed          bool   `json:"installed" facebook:"installed,required"`
-	IsVerified         bool   `json:"is_verified" facebook:"is_verified,required"`
-	Link               string `json:"link" facebook:"link,required"`
-	Locale             string `json:"locale" facebook:"locale,required"`
-	Political          string `json:"political" facebook:"political,required"`
-	Quotes             string `json:"quotes" facebook:"quotes,required"`
-	RelationshipStatus string `json:"relationship_status" facebook:"relationship_status,required"`
-	Religion           string `json:"religion" facebook:"religion,required"`
-	ThirdPartyID       string `json:"third_party_id" facebook:"third_party_id,required"`
+	Name               string `json:"name" facebook:"name"`
+	NameFormat         string `json:"name_format" facebook:"name_format"`
+	Gender             string `json:"gender" facebook:"gender"`
+	Installed          bool   `json:"installed" facebook:"installed"`
+	IsVerified         bool   `json:"is_verified" facebook:"is_verified"`
+	Link               string `json:"link" facebook:"link"`
+	Locale             string `json:"locale" facebook:"locale"`
+	Political          string `json:"political" facebook:"political"`
+	Quotes             string `json:"quotes" facebook:"quotes"`
+	RelationshipStatus string `json:"relationship_status" facebook:"relationship_status"`
+	Religion           string `json:"religion" facebook:"religion"`
+	ThirdPartyID       string `json:"third_party_id" facebook:"third_party_id"`
 	Username           string `json:"username" facebook:"username,required"`
-	Verified           bool   `json:"verified" facebook:"verified,required"`
-	Website            string `json:"website" facebook:"website,required"`
+	Verified           bool   `json:"verified" facebook:"verified"`
+	Website            string `json:"website" facebook:"website"`
 }
