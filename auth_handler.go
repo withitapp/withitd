7bcacf83f336f80c02e0a58c7c5bafd43f72bc07
Oguz Bilgic <fisyonet@gmail.com>
2014-04-04 17:05:48 -0400

    Fix FetchUser()

--- auth_handler.go
+++ auth_handler.go
@@ -3,10 +3,15 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/huandu/facebook"
+	"github.com/withitapp/withitd/dbase"
+	"github.com/withitapp/withitd/fbook"
+	"github.com/withitapp/withitd/model"
 	"net/http"
+	"time"
 )
 
-func NewAuthHandler() http.HandlerFunc {
+func NewAuthHandler(db *dbase.Conn, fb *facebook.App) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		data := r.PostForm
@@ -14,7 +19,7 @@ func NewAuthHandler() http.HandlerFunc {
 		id := data["fb_id"][0]
 		token := data["fb_token"][0]
 
-		user := FetchUser(id, token)
+		user := FetchUser(db, fb, id, token)
 		jsonBlob, err := json.Marshal(user)
 		if err != nil {
 			panic(err)
@@ -25,4 +30,47 @@ func NewAuthHandler() http.HandlerFunc {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(jsonBlob)
 	}
+}
+
+func FetchUser(db *dbase.Conn, fb *facebook.App, id, token string) *model.User {
+	user, err := db.UserTable.SelectBy("fb_id", id)
+	if err == nil && user != nil {
+		println("existing user")
+		return user.(*model.User)
+	}
+
+	session := fb.Session(token)
+
+	res, err := session.Get("/me", nil)
+	err = session.Validate()
+	if err != nil {
+		panic(err)
+	}
+
+	fbUser := fbook.User{}
+	err = res.Decode(&fbUser)
+	if err != nil {
+		panic(err)
+	}
+
+	newUser := &model.User{
+		CreatedAt:  time.Now(),
+		UpdatedAt:  time.Now(),
+		Username:   fbUser.Username,
+		Email:      fbUser.Email,
+		FirstName:  fbUser.FirstName,
+		LastName:   fbUser.LastName,
+		FbID:       id,
+		FbToken:    token,
+		FbSyncedAt: time.Now(),
+	}
+
+	newUserId, err := db.UserTable.Insert(newUser)
+	if err != nil {
+		panic(err)
+	}
+
+	newUser.ID = newUserId
+
+	return newUser
 }
--- main.go
+++ main.go
@@ -3,26 +3,18 @@ package main
 import (
 	"github.com/huandu/facebook"
 	"github.com/withitapp/withitd/dbase"
-	"github.com/withitapp/withitd/fbook"
-	"github.com/withitapp/withitd/model"
 	"net/http"
 	"time"
 )
 
-var (
-	db  *dbase.Conn
-	fb  *facebook.App
-	err error
-)
-
 func main() {
-	db, err = dbase.NewConn("root:root@tcp(localhost:3306)/withit")
+	db, err := dbase.NewConn("root:root@tcp(localhost:3306)/withit")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	fb = facebook.New("514907081964376", "39cb26381e1cb82ae689ff1d7755f577")
+	fb := facebook.New("514907081964376", "39cb26381e1cb82ae689ff1d7755f577")
 	fb.RedirectUri = "http://withitapp.com/"
 
 	userController := &UserController{db}
@@ -31,7 +23,7 @@ func main() {
 	router := NewRouter()
 	router.AddHandler("/users", NewControllerHandler(userController))
 	router.AddHandler("/polls", NewControllerHandler(pollController))
-	router.AddHandler("/auth", NewAuthHandler())
+	router.AddHandler("/auth", NewAuthHandler(db, fb))
 
 	server := &http.Server{
 		Addr:           ":3000",
@@ -43,47 +35,4 @@ func main() {
 
 	println("Starting http server at port 3000 ...")
 	server.ListenAndServe()
-}
-
-func FetchUser(id string, token string) *model.User {
-	user, err := db.UserTable.SelectBy("fb_id", id)
-	if err == nil && user != nil {
-		println("existing user")
-		return user.(*model.User)
-	}
-
-	session := fb.Session(token)
-
-	res, err := session.Get("/me", nil)
-	err = session.Validate()
-	if err != nil {
-		panic(err)
-	}
-
-	fbUser := fbook.User{}
-	err = res.Decode(&fbUser)
-	if err != nil {
-		panic(err)
-	}
-
-	newUser := &model.User{
-		CreatedAt:  time.Now(),
-		UpdatedAt:  time.Now(),
-		Username:   fbUser.Username,
-		Email:      fbUser.Email,
-		FirstName:  fbUser.FirstName,
-		LastName:   fbUser.LastName,
-		FbID:       id,
-		FbToken:    token,
-		FbSyncedAt: time.Now(),
-	}
-
-	newUserId, err := db.UserTable.Insert(newUser)
-	if err != nil {
-		panic(err)
-	}
-
-	newUser.ID = newUserId
-
-	return newUser
 }
