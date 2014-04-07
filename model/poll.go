1fe93f83e37fea64d912afdc169d237082cb037c
scrubsauce <millerjj@usc.edu>
2014-04-07 11:11:43 -0700

    Add model.NewPollFromValues()

--- model/poll.go
+++ model/poll.go
@@ -1,7 +1,10 @@
 package model
 
 import (
+	"errors"
 	"github.com/withitapp/withitd/vldte"
+	"net/url"
+	"strconv"
 	"time"
 )
 
@@ -14,6 +17,37 @@ type Poll struct {
 	Description string    `json:"description"`
 	UserID      int       `json:"user_id"`
 	EndsAt      time.Time `json:"ends_at"`
+}
+
+//Create a Poll object from URL values
+func NewPollFromValues(values url.Values) (*Poll, error) {
+
+	if len(values["title"]) == 0 {
+		return nil, errors.New("title field is empty.")
+	}
+
+	if len(values["description"]) == 0 {
+		return nil, errors.New("description field is empty.")
+	}
+
+	if len(values["user_id"]) == 0 {
+		return nil, errors.New("user_id is empty.")
+	}
+
+	//Convert user ID to string
+	userID, err := strconv.Atoi(values["user_id"][0])
+	if err != nil {
+		return nil, err
+	}
+
+	return &Poll{
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+		Title:       values["title"][0],
+		Description: values["description"][0],
+		UserID:      userID,
+		//EndsAt: value["ends_at"][0],
+	}, nil
 }
 
 func (p *Poll) Validate() error {
